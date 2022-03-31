b7785518029bc34be43bf316d88b5b6a3c2e49c8
Rotimi.E <matanmearoja@yahoo.com>
2022-03-31 14:02:27 -0700

    update the progress js and html file

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+// variables for our timer
+
+// variables for dom (our html elements 
+// example is like document.getElementById)
+var startBtn = document.getElementById("startBtn");
+var questions = document.getElementById("questions");
+var choices = document.getElementById("choices");
+
+
+
+
+function start() {
+    // initiate our quiz and the timer
+    console.log("Test");
+}
+
+function findQuestion() {
+    // go into questions array (questions.js) and display
+}
+
+function trackProgress() {
+    // this function will track the state of the quiz
+    // example: wrong answer is clicked, we subtract time.
+}
+
+
+// addEventListener
+
+startBtn.onclick = start;
