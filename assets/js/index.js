8ea2e3a1bd756d6942f99c15823f533b40733302
Rotimi.E <matanmearoja@yahoo.com>
2022-03-31 15:06:49 -0700

    update the js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,7 @@ var currentQuestion = 0;
 var startBtn = document.getElementById("startBtn");
 var questionsElement = document.getElementById("questions");
 var choices = document.getElementById("choices");
+var timerElement = document.getElementById("time");
 
 
 
@@ -30,8 +31,9 @@ function findQuestion() {
     var currentQ = questions[currentQuestion];
     //display question on screen
     var questionElement = document.getElementById("questionTitle");
-    console.log(currentQ.questions);
+    console.log(currentQ.question);
     questionElement.textContent = currentQ.question;
+    choices.innerHTML = "";
 
     //display choices
     currentQ.choices.forEach(function(choice, i) {
@@ -43,7 +45,7 @@ function findQuestion() {
         // text value of each choice
         choiceBtn.textContent = i + 1 + choice;
 
-        choices.onclick = trackProgress();
+        choiceBtn.onclick = trackProgress;
 
         choices.appendChild(choiceBtn);
     });
@@ -52,12 +54,40 @@ function findQuestion() {
 }
 
 function trackProgress() {
-    // this function will track the state of the quiz
-    // example: wrong answer is clicked, we subtract time.
+
+// when we click on a choice, we want the answer (correct or wrong) then move on.
+// Deduct time if incorrect
+        if (this.value !== questions[currentQuestion].answer) {
+            console.log("wrong");
+            time -= 10;
+
+            if(time < 0) {
+                // we want time at fixed value
+                time = 0;
+            }
+            //something to display time
+            timerElement.textContent = time;
+
+        } else {
+            console.log("correct awnser!");
+        }
+ 
+       currentQuestion++;
+
+       if (currentQuestion === questions.length) {
+           // end quiz
+       } else {
+           findQuestion();
+       }
+
+
+
 }
 
 function clock() {
-    //update time
+    //update time, take our var time and deduct 1
+    time--;
+    timerElement.textContent = time;
 
     //check if time is 0
 }
